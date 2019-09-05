336c463ae2ab047f75b0bfb87e7b83621de7dae9
KerronKing <king_of_palmiste@hotmail.com>
2019-09-05 18:50:53 -0400

    Updated player turn logic

--- bin/main.rb
+++ bin/main.rb
@@ -14,23 +14,34 @@ player2 = Player.new(name2, symbol2)
 
 new_board = Board.new()
 
-puts "Player 1: Where would you like to place #{symbol1}?"
-x1 = gets.chomp.to_i()
-y1 = gets.chomp.to_i()
+4.times {
+    puts "Player 1: Where would you like to place #{symbol1}?"
+    x1 = gets.chomp.to_i()
+    y1 = gets.chomp.to_i()
+    
+    if new_board.position_empty(x1, y1) == false && new_board.invalid_move(x1, y1) == false
+       new_board.update_board(symbol1, x1, y1) 
+    end
+    
+    print new_board.update_board(symbol1, x1, y1)
+    
+    puts "Player 2: Where would you like to place #{symbol2}?"
+    x2 = gets.chomp.to_i()
+    y2 = gets.chomp.to_i()
+    
+    if new_board.position_empty(x2, y2) == false &&  new_board.invalid_move(x2, y2) == false
+        puts new_board.update_board(symbol1, x1, y1) 
+    end
+    
+    print new_board.update_board(symbol2, x2, y2)
+}
 
-if new_board.position_empty(x1, y1) == false && new_board.invalid_move(x1, y1) == false
-   new_board.update_board(symbol1, x1, y1) 
-end
+# Insert check_win function in above loop
+# Use board_not_full function to indicate drawn game
+# Insert reset function
 
-print new_board.update_board(symbol1, x1, y1)
 
-puts "Player 2: Where would you like to place #{symbol2}?"
-x2 = gets.chomp.to_i()
-y2 = gets.chomp.to_i()
 
-if new_board.position_empty(x2, y2) == false &&  new_board.invalid_move(x2, y2) == false
-	puts new_board.update_board(symbol1, x1, y1) 
-end
 
-print new_board.update_board(symbol2, x2, y2)
+
 
